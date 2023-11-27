ffad1fc3b4d19530afd993be3d649882da7fd5f4
SamJ2104 <140335136+SamJ2104@users.noreply.github.com>
2023-11-27 18:37:02 +0000

    Add Website

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'Masker';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'style.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
